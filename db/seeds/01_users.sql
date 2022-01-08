a09b34790cd3545b35655a00e2f36afd7a6c948a
JP <kalambayjp@gmail.com>
2022-01-08 11:30:49 -0800

    started to create seed files for users, categories, products tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,13 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, phone_number, country, city, province, street, post_code) 
+VALUES ('Alice Parker', 'alice@wonderland.ca', 'password', '6043762846', 'Canada', 'Maple Ridge', 'BC', '5088 parker st', 'v2x0j6'),
+('Bruce Banner', 'bbanner@thehulk.ca', 'password', '6043493674', 'Canada', 'Burnaby', 'BC', '3636 willingdon ave', 'v5d7g4'),
+('Barry Allen', 'flash@imveryfast.ca', 'password', '5147369045', 'Canada', 'Montreal', 'QC', '880 Charlevoix st', 'h3k9g4'),
+('Bob Marley', 'something@email.com', 'password', '999-999-99-99', 'Jamaika', 'Kingston', 'Ontario', '1-66 Route', 'r5t7y8'),
+('Jeremy Clarkson', 'grandtour@anymail.com', 'password', '', 'Canada', 'Surrey','BC', '1-This street', 'h4t6c9'),
+('Jim Bim', 'writeme@othermail.com', 'password', '555-678-77-11', 'Canada', 'Regina','SK', '99-That street', 't7n9f4'),
+('Adam Smith', 'adam_smith@gmail.com', 'password', '9008771144', 'Canada', 'Regina', 'SK', 'Albert St', 'S4S3A3' ),
+('Sarah Henderson', 'hisarah@hotmail.com', 'password', '6008572534', 'Canada', 'Regina', 'SK', 'Grant Rd', 'S6S4J1' ),
+('Rogers Steven', 'findsteven@gmail.com', 'password', '5005446843', 'Canada', 'Winnipeg', 'MB', 'Orchard Rd', 'M5Y4T9' ),
+('Rohit Sharma ', 'hellorohit@hotmail.com', 'password', '4212154456', 'Canada', 'Saskatoon', 'SK', '8th Street', 'S7P4T9' );
+
